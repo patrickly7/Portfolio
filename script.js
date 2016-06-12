377874ac1a365a4fd6cca1b299c665459ec40604
Patrick Ly <patrickly7@yahoo.com>
2016-06-12 01:16:59 -0400

    langpic man

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready( function() {
 	$("#langpic").mouseenter( function() {
-		$("#cssinfo").fadeTo('slow', 0)
+		$("#langpic").fadeTo('slow', 0)
 	});
 	$("#langpic").mouseleave( function() {
-		$("#cssinfo").fadeTo('slow', 1)
+		$("#langpic").fadeTo('slow', 1)
 	});
 });
