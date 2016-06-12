13f16121efd87444c1d74378e175394bb48191ac
Patrick Ly <patrickly7@yahoo.com>
2016-06-12 01:24:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 $(document).ready( function() {
-	$("#mainPageContent").mouseenter( function() {
-		$("#mainPageContent").fadeTo('slow', 0)
+	$("#langpic1").mouseenter( function() {
+		$("#langpic1").fadeTo('slow', 0)
 	});
-	$("#mainPageContent").mouseleave( function() {
-		$("#mainPageContent").fadeTo('slow', 1)
+	$("#langpic1").mouseleave( function() {
+		$("#langpic1").fadeTo('slow', 1)
+	});
+	$("#langpic2").mouseenter( function() {
+		$("#langpic2").fadeTo('slow', 0)
+	});
+	$("#langpic2").mouseleave( function() {
+		$("#langpic2").fadeTo('slow', 1)
+	});
+	$("#langpic3").mouseenter( function() {
+		$("#langpic3").fadeTo('slow', 0)
+	});
+	$("#langpic3").mouseleave( function() {
+		$("#langpic3").fadeTo('slow', 1)
+	});
+	$("#langpic4").mouseenter( function() {
+		$("#langpic4").fadeTo('slow', 0)
+	});
+	$("#langpic4").mouseleave( function() {
+		$("#langpic4").fadeTo('slow', 1)
 	});
 });
