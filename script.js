bcb8cc4f6c82bb96fcdfc75421baa73899ceab2b
Patrick Ly <patrickly7@yahoo.com>
2017-07-03 01:16:02 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function() {
-	$("#langpic1, #langpic2, #langpic3, #langpic4").mouseenter( function() {
-		$("#langpic1, #langpic2, #langpic3, #langpic4").fadeTo('slow', 0)
-	});
-	$("#langpic1, #langpic2, #langpic3, #langpic4").mouseleave( function() {
-		$("#langpic1, #langpic2, #langpic3, #langpic4").fadeTo('slow', 1)
-	});
-});
